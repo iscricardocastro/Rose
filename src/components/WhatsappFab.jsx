d5f995bffd07641cf7718c5980192e48d11ad6f6
iscricardocastro <39012117+iscricardocastro@users.noreply.github.com>
2026-06-10 13:43:56 -0600

    Update WhatsappFab.jsx

--- src/components/WhatsappFab.jsx
+++ src/components/WhatsappFab.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function WhatsappFab() {
     return (
         <a
-            href="https://wa.me/528128706467"
+            href="https://wa.me/528123658774"
             target="_blank"
             rel="noreferrer"
             className="fixed bottom-6 right-6 z-50 flex items-center gap-3 rounded-full shadow-lg bg-rosebrand-600 hover:bg-rosebrand-700 text-white px-5 py-3 transition transform hover:scale-105 animate-fade-in"
